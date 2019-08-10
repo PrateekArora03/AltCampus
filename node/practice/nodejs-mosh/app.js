114f4583f0587bdc82d3bb3881ec7e3174a0a0c4
prarora98 <prateekisthebest98@gmail.com>
2019-08-10 23:27:37 +0530

    export function as class

---
+++ node/practice/nodejs-mosh/app.js
@@ -0,0 +1,8 @@
+console.log(module);
+module.exports = function(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  };
+};
