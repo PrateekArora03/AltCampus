b51309ecb3a24f9b2bc9ae78cc392aeca6e84dd7
prarora98 <prateekisthebest98@gmail.com>
2019-09-17 22:55:25 +0530

    update react index with own code

--- React/day-1/src/index.js
+++ React/day-1/src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// function formatName(user) {
+//   return user.firstName + " " + user.lastName;
+// }
+// const getGreeting = user => {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>;
+//   } else return <h1>Hello, Stranger.</h1>;
+// };
+// const user = {
+//   firstName: "Prateek",
+//   lastName: "Arora"
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const element = getGreeting(user);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+setInterval(tick, 1000);
